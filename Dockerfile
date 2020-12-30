8c6614f84ce8c00d89b8a0f0e47c4acbbb5e391b
Thomas Kraaibeek <thomaskraaibeek@gmail.com>
2020-12-30 22:15:22 +0100

    wip progress

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,15 @@ FROM ubuntu:latest
 # Oder git clone git@github.com:ThomasKraaibeek/dovecot-core.git
 ADD assets/ /etc/dovecot-source/  
 ADD entrypoint.sh /
-ADD dovecot.conf /usr/local/etc/dovecot
+ADD certs/localhost.pem /opt/ssl/localhost.pem
+ADD certs/localhost-key.pem /opt/ssl/localhost-key.pem
+ADD dovecot.conf /usr/local/etc/dovecot/dovecot.conf
 ARG DEBIAN_FRONTEND=noninteractive
 ENV TZ=Europe/Berlin
-RUN apt-get update && apt-get install autotools-dev autoconf wget libtool pkg-config gettext pandoc make build-essential git bison flex -y
+RUN apt-get update && apt-get install autotools-dev autoconf wget libtool pkg-config gettext pandoc make build-essential git bison flex openssl libssl-dev dovecot-sieve dovecot-managesieved -y
+
+RUN useradd -ms /bin/bash dovecot || echo "user already exists"
+RUN useradd -ms /bin/bash dovenull || echo "user already exists"
 
 CMD [ "/bin/bash", "entrypoint.sh"]
 #RUN service dovecot restart
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,50 @@
 #!/bin/bash
 
+cp /opt/ssl/localhost.pem /usr/local/etc/dovecot/cert.pem
+cp /opt/ssl/localhost-key.pem /usr/local/etc/dovecot/key.pem
+
 cd /etc/dovecot-source
 
 ./autogen.sh
-./configure --enable-maintainer-mode
+./configure --enable-maintainer-mode --with-ssl=openssl
 make
 make install
 
 cd /usr/local/sbin
 ./dovecot
+service dovecot start
+
+tail -f /var/log/lastlog # TODO enable logging
+
+# TODO PAM https://doc.dovecot.org/configuration_manual/authentication/pam/#authentication-pam
+# TODO CHECK (https://noknow.info/it/os/install_dovecot_from_source)
+
+# conf.d/10-auth.conf
+
+# # Disable LOGIN command and all other plaintext authentications unless SSL/TLS is used.
+# disable_plaintext_auth = yes
+# # plaintext and login are enabled authentication mechanisms.
+# auth_mechanisms = plain login
+# # Include the extension file.
+# !include auth-system.conf.ext
+
+
+
+
+# conf.d/10-mail.conf
+
+# # The mail location is ~/Maildir
+# mail_location = maildir:~/Maildir
+# # Defining namespace sections.
+# namespace inbox {
+#     # There can be only one INBOX
+#     inbox = yes
+# }
+# # System user and group used to access mails.
+# mail_uid = dovecot
+# mail_gid = dovecot
+# protocol !indexer-worker {
+# }
+
+
+
