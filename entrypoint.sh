1b8763c7856bea0d4f0cd70648e55ce4cf922899
Thomas Kraaibeek <thomaskraaibeek@gmail.com>
2021-01-02 09:41:47 +0100

    cleanup and tail real log

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,37 +17,8 @@ cd /usr/local/sbin
 ./dovecot
 service dovecot start
 
-tail -f /var/log/lastlog # TODO enable logging
+tail -f /var/log/dovecot.log 
 
 # TODO PAM https://doc.dovecot.org/configuration_manual/authentication/pam/#authentication-pam
 # TODO CHECK (https://noknow.info/it/os/install_dovecot_from_source)
-
-# conf.d/10-auth.conf
-
-# # Disable LOGIN command and all other plaintext authentications unless SSL/TLS is used.
-# disable_plaintext_auth = yes
-# # plaintext and login are enabled authentication mechanisms.
-# auth_mechanisms = plain login
-# # Include the extension file.
-# !include auth-system.conf.ext
-
-
-
-
-# conf.d/10-mail.conf
-
-# # The mail location is ~/Maildir
-# mail_location = maildir:~/Maildir
-# # Defining namespace sections.
-# namespace inbox {
-#     # There can be only one INBOX
-#     inbox = yes
-# }
-# # System user and group used to access mails.
-# mail_uid = dovecot
-# mail_gid = dovecot
-# protocol !indexer-worker {
-# }
-
-
 
